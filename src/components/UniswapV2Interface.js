c7c531df82ca6aef4045c5df3b3f4e5f2ff78af4
PRAKHAR SRIVASTAVA <130750923+Prakhar-30@users.noreply.github.com>
2025-11-06 17:49:56 +0530

    Update UniswapV2Interface.js

--- src/components/UniswapV2Interface.js
+++ src/components/UniswapV2Interface.js
@@ -7,7 +7,7 @@ const CHAIN_CONFIG = {
   sepolia: {
     chainId: 11155111,
     name: 'Sepolia',
-    rpcUrl: 'https://ethereum-sepolia-rpc.publicnode.com',
+    rpcUrl: 'https://eth-sepolia.g.alchemy.com/v2/QnTJicdL-OSJilaE2y4wVXLy_XuFKmJB',
     explorerUrl: 'https://sepolia.etherscan.io',
     routerAddress: '0xC532a74256D3Db42D0Bf7a0400fEFDbad7694008',
     factoryAddress: '0x7E0987E5b3a30e3f2828572Bb659A548460a3003',
